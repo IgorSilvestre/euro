12f508592167d3176405e665a50813de833484b0
Igor Silvestre <igor@gms.capital>
2025-01-31 16:34:32 -0300

    feat: home page load products with skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
-'use client'
-
 import { ProductsShowcase } from "@/components/ProductsShowcase";
+import { SkeletonCard } from "@/components/ui/skeletonCard";
+import { ReactElement, Suspense } from "react";
 
 export default function Home() {
+  const skeleton: ReactElement[] = []
+
+  function generateSkeleton() {
+  const numberOfSkeletons = 20
+    for (let i = 0; i < numberOfSkeletons; i++) {
+      skeleton.push(
+        <div key={i} className="mx-2 mb-4">
+          <SkeletonCard /> 
+        </div>
+      )
+    }
+  }
+  generateSkeleton()
   return (
     <div>
+    <Suspense fallback={skeleton}>
       <ProductsShowcase />
+    </Suspense>
     </div>
   );
 }
